23497ca595cb2fc51c0f454dc16d3a9faeea63ca
Miroslav <fedechkin.miroslav@yandex.ru>
2025-08-25 15:15:56 +0200

    feat: add exceptions processing

--- api.py
+++ api.py
@@ -4,10 +4,13 @@ import aiohttp
 async def get_info(symbol):
     link = f'https://api.binance.com/api/v3/ticker/24hr?symbol={symbol}'
 
-    async with aiohttp.ClientSession() as session:
-        async with session.get(link) as response:
-            data = await response.json()
-            return data
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(link) as response:
+                data = await response.json()
+                return data
+    except Exception as e:
+        print(f'Error! {e}' )
         
 
 
