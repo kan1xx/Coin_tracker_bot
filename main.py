39cc59bf809eba815f69a2405b4c09a27b796315
Miroslav <fedechkin.miroslav@yandex.ru>
2025-08-25 13:43:02 +0200

    feat: add get info message, beautiful response

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import asyncio
 import logging
 from aiogram import Dispatcher, Bot
-from aiogram.types import Message
+from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton
 from aiogram.filters import Command
-
 from os import getenv
 from dotenv import load_dotenv
-
 from texts import START_TEXT
+from api import get_info
 
 load_dotenv()
 BOT_TOKEN = getenv('BOT_TOKEN')
@@ -15,9 +14,43 @@ BOT_TOKEN = getenv('BOT_TOKEN')
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
+Coins_choose_keyboard = ReplyKeyboardMarkup(
+    keyboard = [
+    [KeyboardButton(text='BTC')]   
+    ],resize_keyboard=True)
+    
+
 @dp.message(Command('start'))
 async def commands_start_handler(message: Message):
-    await message.reply(START_TEXT, parse_mode='HTML')
+    await message.reply(START_TEXT, parse_mode='HTML', reply_markup=Coins_choose_keyboard)
+
+@dp.message()
+async def get_info_about_coin(message: Message):
+    symbol = message.text + 'USDT'
+    data = await get_info(symbol)
+
+    formatted_price = format(float(data['lastPrice']), '.2f')
+    formatted_high_price = format(float(data['highPrice']), '.2f')
+    formatted_low_price = format(float(data['lowPrice']), '.2f')
+    formatted_price_change_percent = format(float(data['priceChangePercent']), '.2f')
+    formatted_volume = format(float(data['volume']), '.2f')
+
+
+
+    await message.reply(
+        f'Info about: <b>{symbol}</b>\n'
+        f'💰 <b>Currently price:</b> ${formatted_price}\n'
+        f'🔺 <b>Highest price for 24 hours:</b> {formatted_high_price}\n'
+        f'🔻 <b>Lowest price for 24 hours:</b> {formatted_low_price}\n'
+        f'📈 <b>Price change percent: </b> {formatted_price_change_percent}\n'
+        f'📊 <b>Volume:</b> {formatted_volume}\n',
+        parse_mode='HTML'
+       )
+
+
+    
+
+
 
 
 
