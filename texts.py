7dee9f2fb9e2740d136b5eb59b50fd2368b2460a
Miroslav <fedechkin.miroslav@yandex.ru>
2025-08-25 15:38:21 +0200

    update: text for 'start' command

--- texts.py
+++ texts.py
@@ -1,5 +1,5 @@
 START_TEXT = '''
 <b>Hello! Welcome to the Coin Tracker Bot! 👋</b>
 Here you can get real-time information about various cryptocurrencies(all information is taken from the Binance exchange).
-To get started, just choose action below: 
+To get started, just enter the coin qoute(it must be like a 'BTC', 'ETH' etc...): 
 '''
